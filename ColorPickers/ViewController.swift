8a7a97a016734ea1774d1429632d742296b6860d
hackeru <hackeru@hackerus-Mac.local>
2017-09-07 17:41:54 +0300

    Initial Commit

---
+++ ColorPickers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorPickers
+//
+//  Created by hackeru on 16 Elul 5777.
+//  Copyright © 5777 com.arichafamily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
